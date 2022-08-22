449a6f20862df40ea1dd0c8e833d35b630c1efee
meyt <pasd3000@gmail.com>
2022-08-22 08:12:26 +0430

    Remove unused code

--- pymt5pure/response.py
+++ pymt5pure/response.py
@@ -8,37 +8,6 @@ from pymt5pure.constants import ret_code_map
 from pymt5pure.exceptions import ResponseError
 
 
-def parse_response(data: bytes, data_ext: bytes = None):
-    data = data.decode("utf-8")
-    fields = data.split("|")
-    params = dict()
-    for field in fields[1:-1]:
-        exploded = field.split("=")
-        if len(exploded) == 2:
-            k, v = field.split("=")
-            params[k.upper()] = v
-
-            if k == "RETCODE":
-                status_code, status_text = v.split(" ")
-                status_code = int(status_code)
-                if status_code != ret_code_map["MT_RET_OK"]:
-                    raise ResponseError(v)
-
-        elif field == "\r\n":
-            continue
-
-        elif not data_ext:
-            data_ext += field
-
-    return dict(
-        cmd=fields[0],
-        params=params,
-        status_code=status_code,
-        status_text=status_text,
-        binary=data_ext,
-    )
-
-
 class Response:
     def __init__(self, data: bytes):
         self.data = data
